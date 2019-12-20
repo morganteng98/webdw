5088b9d31bc6ba4cb49c9c93700870cf36b91280
taufikrahman <rahmantaufikur10@gmail.com>
2019-12-20 22:23:07 +0700

    pull rek

--- DW/toko.php
+++ DW/toko.php
@@ -1,13 +1,40 @@
 <?php include "kategori_head.php"; ?>
-<span class="badge badge-warning" style="font-size: 20px;margin-bottom: 5px">Semua Kategori</span>
+<div class="container" style="background-color: gray">
+ <div class="nav-scroller bg-black py-1 mb-2">
+    <ul class="nav d-flex justify-content-between">
+      <li class="dropdown ">
+      	<a href="#" class="dropdown-toggle p-2 text-light" data-toggle="dropdown">Alat Musik <b class="fa fa-caret-down"></b></a>
+						<ul class="dropdown-menu"style="background-color: gray">
+							<li><a href="#" class="p-2 text-light">Gitar</a></li>
+							<li><a href="#" class="p-2 text-light">Keyboard</a></li>
+							<li><a href="#" class="p-2 text-light">Drum</a></li>
+						</ul>
+		</li>
+		<li class="dropdown ">
+      	<a href="#" class="dropdown-toggle p-2 text-light" data-toggle="dropdown">Sparepart <b class="fa fa-caret-down"></b></a>
+						<ul class="dropdown-menu"style="background-color: gray">
+							<li><a href="#" class="p-2 text-light">Gitar</a></li>
+							<li><a href="#" class="p-2 text-light">Keyboard</a></li>
+							<li><a href="#" class="p-2 text-light">Drum</a></li>
+						</ul>
+		</li>
+      <li class="dropdown ">
+      	<a href="#" class="dropdown-toggle p-2 text-light" data-toggle="dropdown">Aksesoris<b class="caret"></b></a>
+		</li>
+		<li class="dropdown ">
+      	<a href="#" class="dropdown-toggle p-2 text-light" data-toggle="dropdown">Alat Elektronik<b class="caret"></b></a>
+		</li>
+    </ul>
+  </div>
+  </div>
 
 
-					<div class="row" style="padding: 100px">
+					<div class="row" style="padding: 20px; ">
 
 				<?php $ambil = $koneksi ->query("SELECT * FROM produk  ORDER BY jenis_produk"); ?><!-- dari database belajar -->
 				<?php while($perproduk = $ambil->fetch_assoc()){  ?>
 				
-				<div class="col-md-3" style="margin-bottom: 50px;text-align: center">
+				<div class="col-md-3" style="margin-bottom: 50px;text-align: center ;">
 					<div class="thumnail" style="background-color: #eaeaea; border: solid #eaeaea; ">
 					<img src="foto/<?php echo $perproduk['foto_produk'];?>" alt"">
 				<div class="caption">
@@ -23,10 +50,8 @@
 	
 
 						
-				
 			</div>	
 			</div>	
 		</div>	
 </section>
-
-<?php include 'kategori_tail.php'; ?>
+<?php include 'footer.php'; ?>
