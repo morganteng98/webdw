2f887b151ff589bd7e4d8bf3f04f16e8e3cda114
Morgan Ardianto <45646621+morganteng98@users.noreply.github.com>
2020-01-09 22:15:55 +0700

    Update toko.php
    taufik style

--- DW/toko.php
+++ DW/toko.php
@@ -2,22 +2,21 @@
 		include 'nav_taufik.php';
  ?>
 
-					<div class="row" style="padding: 100px">
+<div class="row" style="padding: 100px;">
 
-				<?php $ambil = $koneksi ->query("SELECT * FROM produk  ORDER BY jenis_produk"); ?><!-- dari database belajar -->
-				<?php while($perproduk = $ambil->fetch_assoc()){  ?>
-				
-				<div class="col-md-3" style="margin-bottom: 50px;text-align: center;">
-					<div class="thumnail" style="background-color: #eaeaea; border: solid #eaeaea; ">
-					<img src="foto/<?php echo $perproduk['foto_produk'];?>" alt=@"">
-A
-				<div class="caption" style="background-color: white;">
-					<h5 style="margin-bottom: 5px"><?php echo $perproduk['nama_produk'];?></h5>
-					<h5 style="background-color: orange;margin-bottom: 10px"><b>Harga</b> Rp.<?php echo number_format($perproduk['harga_produk']);?></h5>
+	<?php $ambil = $koneksi ->query("SELECT * FROM produk  ORDER BY jenis_produk"); ?><!-- dari database belajar -->
+	<?php while($perproduk = $ambil->fetch_assoc()){  ?>
+			
+	<div class="col-md-3" style="margin-bottom: 50px;text-align: center;">
+		<div class="thumnail" style="background-color: #eaeaea; border: solid #eaeaea;border-radius: 10px;height: 100%">
+			<img src="foto/<?php echo $perproduk['foto_produk'];?>" style="height: 150px;width: 200px">
 
-				
-					<a href="beli.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-primary">Beli</a>
-<a href="detail.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-info">Detail</a>`	
+		<div class="caption" style="background-color: white">
+			<h5 style="margin-bottom: 5px"><?php echo $perproduk['nama_produk'];?></h5>
+				<h5 style="background-color: orange;margin-bottom: 10px"><b>Harga</b> Rp.<?php echo number_format($perproduk['harga_produk']);?></h5>
+	
+	<a href="beli.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-primary">Beli</a>
+	<a href="detail.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-info">Detail</a>`	
 
 				</div>
 				</div>
@@ -31,4 +30,6 @@ A
 			</div>	
 		</div>	
 </section>
-<?php include 'kategori_tail.php'; ?>
+<div class="cpy-right text-center">
+	<p>DW MUSIK <br/>
+</div>
