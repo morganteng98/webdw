94ffb489a486ad9baa0092d878e02a4f47855991
Morgan Ardianto <45646621+morganteng98@users.noreply.github.com>
2020-01-13 06:57:49 +0700

    perbaikan
    -

--- administrator/login.php
+++ administrator/login.php
@@ -18,8 +18,6 @@ $koneksi = new mysqli("localhost","root","","belajar");
   <title>SB Admin 2 - Login</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -39,7 +37,9 @@ $koneksi = new mysqli("localhost","root","","belajar");
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
--- nav_taufik.php
+++ nav_taufik.php
@@ -5,28 +5,32 @@
       <li class="dropdown ">
       	<a href="toko.php" class="dropdown-toggle p-2 text-light">Semua Kategori<b class="caret"></b></a>
 		</li>
-
+<!-- 
       <li class="dropdown ">
       	<a href="ktgr_alatmusik.php" class="dropdown-toggle p-2 text-light">Alat Musik <b class="fa fa-caret-down"></b></a>
-						<ul class="dropdown-menu"style="background-color: orange">
-							<li><a href="#" class="p-2 text-light">JAKET</a></li>
-							<li><a href="#" class="p-2 text-light">OLAHRAGA</a></li>
-						</ul>
+			<ul class="dropdown-menu"style="background-color: orange">
+				<li><a href="#" class="p-2 text-light">JAKET</a></li>
+				<li><a href="#" class="p-2 text-light">OLAHRAGA</a></li>
+			</ul>
 		</li>
-
-
 		<li class="dropdown ">
-
       	<a href="ktgr_sparepart.php" class="dropdown-toggle p-2 text-light">Sparepart <b class="fa fa-caret-down"></b></a>
 
-						<ul class="dropdown-menu"style="background-color: orange">
-							<li><a href="#" class="p-2 text-light">JAKET</a></li>
-							<li><a href="#" class="p-2 text-light">OLAHRAGA</a></li>
-						</ul>
+			<ul class="dropdown-menu"style="background-color: orange">
+				<li><a href="#" class="p-2 text-light">JAKET</a></li>
+				<li><a href="#" class="p-2 text-light">OLAHRAGA</a></li>
+			</ul>
+		</li> -->
+     	 <li class="dropdown ">
+      		<a href="ktgr_alatmusik.php" class="dropdown-toggle p-2 text-light">Alat Musik<b class="caret"></b></a>
 		</li>
-      <li class="dropdown ">
-      	<a href="ktgr_asesoris.php" class="dropdown-toggle p-2 text-light">Aksesoris<b class="caret"></b></a>
+		 <li class="dropdown ">
+      		<a href="ktgr_sparepart.php" class="dropdown-toggle p-2 text-light">Sparepart<b class="caret"></b></a>
 		</li>
+		 <li class="dropdown ">
+      		<a href="ktgr_asesoris.php" class="dropdown-toggle p-2 text-light">Aksesoris<b class="caret"></b></a>
+		</li>
+
 		<li class="dropdown ">
       	<a href="ktgr_elektronik.php" class="dropdown-toggle p-2 text-light">Alat Elektronik<b class="caret"></b></a>
 		</li>
